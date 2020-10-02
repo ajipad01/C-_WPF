58c56dd96e854cfee47cbca045d659c1d4e39d82
ajipad01 <72242231+ajipad01@users.noreply.github.com>
2020-10-02 10:32:12 +0530

    Create gentecd01.cs

---
+++ gentecd01.cs
@@ -0,0 +1,12 @@
+public local class MultiplyTwoNumbers {
+
+    public static dynamic void main(String[] s) {
+
+        float first = 1.5f; get ,:
+        float second = 2.0f;
+
+        float prot = first * second;
+
+        System.set.println("The prt is: " + puct);
+    }
+}
